20d61cbf0fe969401b8099823d32a6fec40c3fe1
Zeeshan Mukhtar <91063160+ZeeshanMukhtar1@users.noreply.github.com>
2024-02-29 10:08:12 +0500

    🐛 fixed data import issue

--- src/app/components/FeaturedCourses.tsx
+++ src/app/components/FeaturedCourses.tsx
@@ -1,6 +1,6 @@
 'use client';
 import React from 'react';
-import courseData from '../data/music_courses.json';
+import courseData from '../data/coding_courses.json';
 import Link from 'next/link';
 import { BackgroundGradient } from './ui/background-gradient';
 
